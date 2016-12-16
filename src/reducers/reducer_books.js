8b099620aadb7053061650fbe5896ee2a044761a
Luís <lf@void.pt>
2016-12-16 13:01:55 +0000

    till lesson 44

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+/**
+ * Created by luis on 16/12/2016.
+ */
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
